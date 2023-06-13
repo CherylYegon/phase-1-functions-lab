e43793d75753623dc0915050b10e95972dffc9b3
CherylYegon <Cyegon2023@gmail.com>
2023-06-13 11:36:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+// index.js
+
+
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42);
+  }
+  
+  function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(start - destination);
+    return blocks * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else if (distance > 2500) {
+      return 'cannot travel that far';
+    }
+  }
+  
+
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice,
+  };
+  
+  
+  // Export the functions for testing or external usage if needed
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice,
+  };
+  
